ec0d81a19c3653fbea1e9f245d29f8a7e11d556d
NguyenPhuc123789 <cute.hotelmanage@gmail.com>
2022-12-31 20:10:59 -0500

    first commit

---
+++ day8.js
@@ -0,0 +1,29 @@
+// function tinhtong(a,b){
+//     if(a + b == 10){
+//         true_anwer()
+//     }else{
+//         false_anwer()
+//     }
+// };
+
+// function true_anwer(){
+//     console.log("true")
+// }
+// function false_anwer(){
+//     console.log("false")
+// }
+// tinhtong( 2 , 8)
+let menu = ["rau luoc" , "thit kho", "ga luoc"];
+
+
+function addmenu(c){
+   if(input ==c){
+    window.alert("Thêm móm");
+   } else{
+    window.alert("sai");
+   }
+}
+function add(){
+    consol.log("thêm móm")
+}
+addmenu( c , u , r , d);
